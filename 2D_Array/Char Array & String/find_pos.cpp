b22c25f7f96083698a8cbed5746e505e54ab61d8
Aditya-kr43 <kumaraditya912654@gmial.com>
2026-06-16 20:04:33 +0530

    New  Chapter

---
+++ 2D_Array/Char Array & String/find_pos.cpp
@@ -0,0 +1,11 @@
+// Find the position of the char
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch = 'f';
+    int position = ch-'a';
+    cout << position << endl;
+    return 0;
+
+}
